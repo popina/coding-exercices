8d7c05f9818930282dfc435b7c9fe551193a3b99
Masto <fonkymasto@gmail.com>
2023-06-06 18:24:16 +0200

    exercices 03 unit testing

--- 03-unit-testing/02-transform-data/transform-data.test.ts
+++ 03-unit-testing/02-transform-data/transform-data.test.ts
@@ -4,3 +4,14 @@ import { formatProductDescriptions } from "./";
 /**
  * Write a test that verifies that the formatProductDescriptions function correctly formats product descriptions.
  */
+
+test('formatProductDescriptions function correctly formats product descriptions', () => {
+    const result = formatProductDescriptions(products)
+
+    expect(result.length).toBe(6);
+
+    expect(result[0]).toBeDefined
+    expect(result[0]).toBe("Product 1: 'Coca-Cola' is available for only $99.99")
+    expect(result[1]).toBeDefined
+    expect(result[1]).toBe("Product 2: 'Pepperoni Pizza' is available for only $50.00")
+});
--- 03-unit-testing/03-nested-data/nested-data.test.ts
+++ 03-unit-testing/03-nested-data/nested-data.test.ts
@@ -4,3 +4,16 @@ import { addUserTotalOrderValue } from ".";
 /**
  * Write a unit test for the addUserTotalOrderValue function.
  */
+
+test('addUserTotalOrderValue function correctly reduce objects', () => {
+    const result = addUserTotalOrderValue(users)
+
+    expect(result.length).toBe(4);
+
+    expect(result[0]).toBeDefined
+    expect(result[0].name).toBe("John Doe")
+    expect(result[0].totalOrderValue).toBe(300)
+    expect(result[1]).toBeDefined
+    expect(result[1].name).toBe("Jane Doe")
+    expect(result[1].totalOrderValue).toBe(700)
+});
